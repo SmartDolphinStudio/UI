b312da3644239434f754b954ed7056ad137e603b
SmartDolphinStudio <SmartDolphinStudio@gmail.com>
2024-05-13 21:59:52 +0800

    feat: add src main

---
+++ smart-dolphin-verification/src/main.tsx
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// Copyright (C) 2000-2026 Astraeus <SmartDolphinStudio@gmail.com>
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
