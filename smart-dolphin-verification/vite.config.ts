442a3efbf12743da7d271a7a1f741f10f50e4dd2
SmartDolphinStudio <SmartDolphinStudio@gmail.com>
2024-05-21 18:18:27 +0800

    feat: add smart dolphin verification vite config

---
+++ smart-dolphin-verification/vite.config.ts
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// Copyright (C) 2000-2026 Astraeus <SmartDolphinStudio@gmail.com>
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+});
